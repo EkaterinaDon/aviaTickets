485488a8c8dfd5d5f7f48fb0d157b9d1c424952e
Ekaterina Donskaya <katrine.d@gmail.com>
2021-03-03 20:34:01 +0200

    Lesson_1

---
+++ aviaTickets/Controllers/TicketsViewController.m
@@ -0,0 +1,31 @@
+//
+//  TicketsViewController.m
+//  aviaTickets
+//
+//  Created by Ekaterina on 3.03.21.
+//
+
+#import "TicketsViewController.h"
+
+@interface TicketsViewController ()
+
+@end
+
+@implementation TicketsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor: [UIColor redColor]];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
