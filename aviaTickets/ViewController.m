018a70d26cba8effb98620a6f04cee5fee3c3d9a
Ekaterina Donskaya <katrine.d@gmail.com>
2021-03-03 17:09:55 +0200

    Initial Commit

---
+++ aviaTickets/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  aviaTickets
+//
+//  Created by Ekaterina on 3.03.21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor: [UIColor orangeColor]];
+}
+
+
+@end
